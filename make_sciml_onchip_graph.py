439e91e44ad1b66f8db2bb3d77c62393eb095924
Olivia Weng <oliviajweng@gmail.com>
2024-02-27 23:57:00 -0800

    add model bandwidth to each benchmark where baseline model info is available

--- make_sciml_onchip_graph.py
+++ make_sciml_onchip_graph.py
@@ -6,26 +6,93 @@ import mplhep as hep
 import numpy as np
 import math
 
+# Example
+# 10 MB MLP model
+# 10 MB x 10 MB = 1e7 x 1e7 = 1e14 Ops required
+#                             -----------------
+# 1e10 Ops x 1e-3 s x 1e10 B/s x 1e-3 s
+# --------            --------
+# plot                plot
+# = 1e7 Op x 1e7 B = 1e14 Ops * B
+
+# 100 KB MLP model
+# 100 KB x 100 KB = 1e5 x 1e5 = 1e10 Ops required
+# 1e10 Ops x 1e-5 s x 1e10 B/s x 1e-5
+# --------            --------
+# plot                plot
+# = 1e5 x 1e5 = 1e10 Ops
+
+# LHC Sensor
+# 1 KB MLP model
+# 1 KB * 1 KB = 1e3 * 1e3 = 1e6 Ops required
+# 25e12 input data rate (lower bound)
+# 25e12 B/s * 1e3 B = 25e15 B/s required
+# 1e-8 s requirement
+#
+# 25e15 B/s * 1e-8 s = 25e7 B
+# 1e6 Ops * 1e-8 = 1e-2 Ops
+# = 25e7 B * 1e-2 Ops = 25e5 Ops * B
+#
+# 100e12 input data rate (upper bound)
+# 100e12 B/s * 1e3 B = 100e15 B/s required
+# 1e-8 s requirement
+#
+# 100e15 B/s * 1e-8 s = 100e7 B
+# 1e6 Ops * 1e-8 = 1e-2 Ops
+# = 100e7 B * 1e-2 Ops = 100e5 Ops * B = 1e7
+
+####
+# New calculations (input bandwidth + model wt bandwidth)
+# LHC Sensor (ECON)
+# 2k weights / 25 ns = 80e9
+
+# LHC Trigger (Jet tagger)
+# 4k weights / 150 ns = 26e9
+
+# Beam control
+# 35k weights / 4 us = 9e9 
+
+# Plasma control
+# 13k weights / 7 us = 2e9 
+
+# KWS
+# 3e5 weights / 1 ms = 300e6
+
+# AD
+# 2e4 weights / 1 ms = 20e6 
+
 # label: data rate lower bound [B/s], data rate upper bound [B/s], latency lower bound [s], latency upper bound [s]
 input_dict = {
-    "LHC sensor": [25e12, 100e12, 10e-9, 25e-9],
+    # LHC sensor
+    # "LHC sensor": [25e12, 100e12, 10e-9, 25e-9],
+    # LHC sensor + model
+    "LHC sensor": [26e12, 101e12, 10e-9, 25e-9],
     # "LHC near-sensor": [48 * 40e6, 48 * 40e6, 25e-9, 100e-9],
-    "LHC trigger": [32 * 40e6, 32 * 40e6, 100e-9, 5e-6],
+    # "LHC trigger": [32 * 40e6, 32 * 40e6, 100e-9, 5e-6],
+    # LHC trigger + model
+    "LHC trigger": [32 * 40e6 + 26e9, 32 * 40e6 + 26e9, 100e-9, 5e-6],
     # "Beam Control": [3e3 * 15, 3e3 * 15, 100e-6, 5e-3], # Booster
-    "Beam control": [3e3 * 15, 3e3 * 15, 100e-6, 5e-3], # Booster control
-    "Magnet quench": [3e6, 3e6, 100e-6, 100e-6],  # Quench
+    # Booster control + model
+    "Beam control": [3e3 * 15 + 9e9, 3e3 * 15 + 9e9, 100e-6, 5e-3], # Booster
+    # "Magnet quench": [3e6, 3e6, 100e-6, 100e-6],  # Quench
     # "Pixel": [1.28e9/8 * 40/.75, 4*1.28e9/8 * 40/.75, 10e-9, 25e-9],
     # "DUNE": [1e9, 10e9, 1, 5*60],
-    "DUNE readout": [0.8e9, 0.8e9, 1e-6, 1e-6],
+    # "DUNE readout": [0.8e9, 0.8e9, 1e-6, 1e-6],
     # "Quantum": [1e9, 7e9, 500e-9, 1000e-9], 
     # "Quantum": [40e9, 40e9, 100e-9, 1e-6], 
-    "Qubit Readout": [9e9, 9e9, 100e-9, 100e-9], 
-    "EIC trigger": [7.5e9, 7.5e9, 500e-9, 500e-9], 
-    "X-ray diffraction": [10e6, 100e6, 1e-6, 20e-6],
-    "Electron microscopy": [0.6e9, 0.6e9, 50e-6, 50e-6],
-    "Plasma control": [3e9, 3e9, 5e-6, 20e-6],
-    "Neuro": [5e6, 5e6, 1e-3, 1e-3],
-    "Internet-of-things": [3e3 / 100e-3, 3e3 / 1e-3, 1e-3, 100e-3],
+    # "Qubit Readout": [9e9, 9e9, 100e-9, 100e-9], 
+    # "EIC trigger": [7.5e9, 7.5e9, 500e-9, 500e-9], 
+    # "X-ray diffraction": [10e6, 100e6, 1e-6, 20e-6],
+    # "Electron microscopy": [0.6e9, 0.6e9, 50e-6, 50e-6],
+    # "Plasma control": [3e9, 3e9, 5e-6, 20e-6],
+    # Plasma control + model
+    "Plasma control": [3e9 + 2e9, 3e9 + 2e9, 5e-6, 20e-6],
+    # "Neuro": [5e6, 5e6, 1e-3, 1e-3],
+    # "Internet-of-things": [3e3 / 100e-3, 3e3 / 1e-3, 1e-3, 100e-3],
+    # IoT + KWS
+    "Keyword Spotting": [3e3 / 100e-3 + 3e8, 3e3 / 1e-3 + 3e8, 1e-3, 100e-3],
+    # IoT + AD
+    "Anomoly Detection": [3e3 / 100e-3 + 20e6, 3e3 / 1e-3 + 20e6, 1e-3, 100e-3],
     "Mobile devices": [1e3 / 100e-3, 1e3 / 40e-3, 40e-3, 100e-3],
     # "FF": [10e12, 10e13, 10e-9, 50e-9],
 }
@@ -110,14 +177,21 @@ for xloi, xhii, yloi, yhii, l, c in zip(xlo, xhi, ylo, yhi, labels, colors):
     sz=20.
     if "Internet-of-things" in l:
         # ax.text(xi * 5, yi * 2, l, color=c, size=sz)
-        ax.text(xi * 5, yi * 2, "IoT", color=c, size=sz)
+        ax.text(xi * 1.1, yi * 2, "IoT", color=c, size=sz)
+    if "Keyword Spotting" in l:
+        # ax.text(xi * 5, yi * 2, l, color=c, size=sz)
+        ax.text(xi * 1.1, yi * 2, "Keyword Spotting", color=c, size=sz)
+    elif "Anomoly Detection" in l:
+        # ax.text(xi * 5, yi * 2, l, color=c, size=sz)
+        ax.text(xi * 1.1, yi * 3, "Anomoly Detection", color=c, size=sz)
     elif "Mobile devices" in l:
         # ax.text(xi * 2, yi * 2, l, color=c, size=sz)
-        ax.text(xi * 2, yi * 2, "Mobile\ndevices", color=c, size=sz)
+        ax.text(xi * 2, yi * 0.5, "Mobile\ndevices", color=c, size=sz)
     elif "Beam control" in l:
-        ax.text(xi / 5e3, yi * 2, l, color=c, size=sz)
+        ax.text(xi * 0.5, yi * 0.5, l, color=c, size=sz)
     elif "LHC sensor" in l:
-        ax.text(xi * 3, yi / 2, l, color=c, size=sz)
+        # ax.text(xi * 3, yi / 2, l, color=c, size=sz)
+        ax.text(xi * 3, yi / 2, "LHC\nsensor", color=c, size=sz)
     elif "LHC near-sensor" in l:
         ax.text(xi / 20, yi * 3.5, "LHC", color=c, size=sz)
         ax.text(xi / 20, yi * 1.5, "near-sensor", color=c, size=sz)
@@ -129,7 +203,7 @@ for xloi, xhii, yloi, yhii, l, c in zip(xlo, xhi, ylo, yhi, labels, colors):
     elif "Qubit Readout" in l:
         ax.text(xi / 10, yi * 1.9, l, color=c, size=sz)
     elif "Plasma control" in l:
-        ax.text(xi * 3, yi * 1, l, color=c, size=sz)
+        ax.text(xi, yi * 0.3, l, color=c, size=sz)
     elif "DUNE readout" in l:
         ax.text(xi / 5e2, yi / 3, l, color=c, size=sz)
     elif "DUNE" in l:
@@ -163,15 +237,18 @@ ax.text(1, 150e12, "FF", color="blue", style="italic", fontsize=18)
 
 # 100 KB model size line (assume fully-connected layer)
 # so there are 100k X 100k ops = 10^10 Ops relative to memory bandwidth
-x_100kb = [1e-9, 1e-6, 10e-6, 10e-3, 10, 1e3]
-y_100kb = [1e15, 1e12, 100e9, 10e7, 1e5, 1e3]
+# OLD BUGGY
+# x_100kb = [1e-9, 1e-6, 10e-6, 10e-3, 10, 1e3]
+# y_100kb = [1e15, 1e12, 100e9, 10e7, 1e5, 1e3]
+x_100kb = [1e-9, 1e-6, 1e-5, 10e-3, 10, 1e3]
+y_100kb = [1e14, 1e11, 1e10, 10e6, 1e4, 1e2]
 
 # 10 MB model
 x_10mb = [1e-9, 100e-9, 1e-6, 10e-6, 1e-3, 1e3, 1e5]
 y_10mb = [1e16, 100e12, 10e12, 1e12, 10e9, 1e4, 1e2]
 
 ax.plot(x_100kb, y_100kb, linestyle="-", color="purple")
-loc_100kb = np.array((1e-1, 10e4))
+loc_100kb = np.array((1e-2, 10e4))
 angle_100kb = 310
 ax.text(
     *loc_100kb, 
@@ -201,8 +278,8 @@ ax.fill_between(
     y_10mb[:5], 
     [10e9] * 5, 
     interpolate=False, 
-    color="cyan", 
-    alpha=0.5,
+    color="red", 
+    alpha=0.15,
     label="On-chip inference required"
 )
 
